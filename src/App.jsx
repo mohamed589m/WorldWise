2fe7014661c251b737738ecc854aa924103d5f2d
mohamed589m <mohamed53634372@gmail.com>
2024-09-28 12:05:53 +0300

    WorldWise Application

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Homepage from "./pages/HomePage";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+// import Login from "./pages/Login";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Login = lazy(() => import("./pages/Login"));
+
+// dist/assets/index-9e74452f.css   30.48 kB │ gzip:   5.07 kB
+// dist/assets/index-540aa79b.js   506.54 kB │ gzip: 147.82 kB
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="product" element={<Product />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,17 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return <nav className={styles.nav}>
+    <ul>
+      <li>
+        <NavLink to="cities">Cities</NavLink>
+      </li>
+      <li>
+        <NavLink to="countries">countries</NavLink>
+      </li>
+    </ul>
+  </nav>;
+}
+
+export default AppNav;
